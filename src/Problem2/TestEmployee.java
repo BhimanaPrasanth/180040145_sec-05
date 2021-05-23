ae536925f6c80c3d6137c34b739cbe3e32279deb
Bhimana prasanth <71579757+PrasanthBhimana@users.noreply.github.com>
2021-05-23 22:28:13 +0530

    .

--- src/Problem2/TestEmployee.java
+++
@@ -1,9 +0,0 @@
-package Problem2;
-
-public class TestEmployee {
-	public static void main(String[] args) {
-		Employee e = new Employee("prasanth", 5000000, 2021, "321a2sd1321ad");
-		System.out.println(e);
-	}
-
-}
